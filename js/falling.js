a90ca183a88c2e656cff2126b6759bbf27393e9a
Juliet Brown <julietbrown84@gmail.com>
2015-06-29 11:01:51 +1200

    falling is now working

---
+++ js/falling.js
@@ -0,0 +1,30 @@
+$(window).ready(function() {
+  $("h1").animate({top: '300px',right: '300px'});
+ });
+
+function fallingSnow() {
+
+  var snowflake = $('<div class="snowflakes"></div>');
+  $('#snowZone').prepend(snowflake);
+  snowX = Math.floor(Math.random() * $('#site').width());
+  snowSpd = Math.floor(Math.random() + 5000);
+
+  snowflake.css({'left':snowX+'px'});
+  snowflake.animate({
+      top: "500px",
+      opacity : "0",
+
+  }, snowSpd, function(){
+      $(this).remove();
+      fallingSnow();
+  });
+
+}
+var timer = Math.floor(Math.random() +1000);
+
+window.setInterval(function(){
+  fallingSnow();
+}, timer);
+
+
+$(document).ready(main);
